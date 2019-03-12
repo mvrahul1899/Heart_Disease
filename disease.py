864b2d0c8d26da72aaac457a65a00a886eb6b8fa
Rahul Muppirala <47323192+mvrahul1899@users.noreply.github.com>
2019-03-12 22:44:40 +0530

    Update disease.py

--- disease.py
+++ disease.py
@@ -5,14 +5,6 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-import os
-print(os.listdir("../input"))
-
-# Any results you write to the current directory are saved as output.
-
 # Importing the dataset
 dataset = pd.read_csv('../input/heart.csv')
 X = dataset.iloc[:, 0:13].values
